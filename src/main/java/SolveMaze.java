0ae2f0cbce9d06f25ce920e4d354918fdf0110ac
Advay Das <advayd2@illinois.edu>
2018-09-12 20:47:20 -0500

    commit

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,13 +34,34 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+
+            if (maze.isFinished() == false) {
+                if (maze.canMove() == false) {
+                    maze.turnRight();
+
+
+                    if (maze.canMove() == false) {
+                        maze.turnLeft();
+                        maze.turnLeft();
+
+                        if (maze.canMove() == false) {
+                            maze.turnLeft();
+                        }
+                    }
+                } else {
+                    maze.move();
+                }
+
+            }
             // Implement your maze solving algorithm here
+
+
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
         }
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
-        }
     }
 }
